e08b3dd8326b56564e0ff2c28cf095f970271994
ReneWebs <72133002+renatombomio@users.noreply.github.com>
2026-09-09 10:53:37 +0200

    refactor(phase-8): clean filter imports

--- js/state/app.js
+++ js/state/app.js
@@ -1,8 +1,5 @@
 import {
     loadPokemonCollection,
-    loadPokemonByType,
-    loadPokemonByPokedex,
-    loadPokemonByGeneration,
     loadPokemonReferencesByType,
     loadPokemonReferencesByPokedex,
     loadPokemonReferencesByGeneration,
